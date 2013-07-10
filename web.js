7a305b010bc05b4215767f352098a8b07ea2f48c
Ubuntu <ubuntu@ip-10-195-11-202.ec2.internal>
2013-07-10 02:35:46 +0000

    Read output from file

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var fileName = 'index.html';
+fs.stat(fileName, function(error, stats) {
+  fs.open(fileName, "r", function(error, fd) {
+    var buffer = new Buffer(stats.size);
+    fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+      var data = buffer.toString("utf8", 0, buffer.length);
+      fs.close(fd);
+      var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+      app.get('/', function(request, response) {
+        response.send(data);
+      });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+      var port = process.env.PORT || 5000;
+      app.listen(port, function() {
+        console.log("Listening on " + port);
+      });
+    });
+  });
 });

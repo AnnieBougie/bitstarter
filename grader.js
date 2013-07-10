e1daee3aca3e41cc5cc1fbc9f141ec16a17a8e7a
Ubuntu <ubuntu@ip-10-195-11-202.ec2.internal>
2013-07-10 11:55:44 +0000

    Start changes to grader to allow urls for checking

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +56,12 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var urlFileExists = function(url, checksfile) {
+    rest.get(url).on('complete', function(result) {
+      return result;
+    });
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,8 +72,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url of index.html', clone(urlFileExists), 'http://protected-spire-9170.herokuapp.com/index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson;
+    if (program.file) {
+      checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    if (program.url) {
+      checkJson = urlFileExists(program.url, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
